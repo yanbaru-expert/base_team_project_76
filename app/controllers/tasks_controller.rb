6e7d289cdb8e77ba55fd986ad409503b0bb40669
andre-883 <harleyy883@gmail.com>
2021-08-31 04:32:59 +0900

    新規投稿機能 new, create を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,14 @@
 class TasksController < ApplicationController
+    def new
+        @task = Task.new
+    end
+
+    def create
+        Task.create(task_params)
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :age)
+    end
 end
